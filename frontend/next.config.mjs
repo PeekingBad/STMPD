5c192958cb3d3f5a97afbfc3ed29cd9cfca47dcb
Semmy Verdonschot <hello@semmyverdonschot.com>
2025-12-11 20:25:02 +0100

    improve: add secure prod backend, new server, metadata, README + MIT

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,8 +7,8 @@ const nextConfig = {
         hostname: "placehold.co",
       },
       {
-        protocol: "http",
-        hostname: "84.26.27.68",
+        protocol: "https",
+        hostname: "strapi.cytric.nl",
       },
     ],
   },
