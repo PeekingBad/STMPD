a07c11c5974d24b567c4c1dbca0d8140d2b15cc6
Verwegen,Daniell,D. <danielverwegen@live.nl>
2026-01-13 10:05:35 +0100

    fix: hero

--- frontend/src/components/layout/hero/hero.desktop.tsx
+++ frontend/src/components/layout/hero/hero.desktop.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client';
 
 import { Container } from "@/components/Container";
 import Icon from "@/components/ui/icon.component";
 import React from "react";
+
 
 const HeroDesktop = () => {
   return (
@@ -34,27 +35,8 @@ const HeroDesktop = () => {
           <span>Studio</span>
           </p>
         </div>
-        <div className="absolute bottom-9 inset-x-0">
-          <div className="flex flex-col md:grid md:grid-cols-3 items-center">
-            <p className="hidden md:block text-left md:max-w-[21.25rem]">
-              STMPD recording studios is Amsterdam&apos;s largest recording
-              studio facility. Designed as the ultimate playground for music and
-              audio post-production.
-            </p>
-
-            <div className="flex justify-center h-full items-end order-3 md:order-2">
-              <Icon icon="arrow-down" />
-            </div>
-
-            <p className="flex flex-col text-center md:text-right uppercase leading-tight font-maharlika text-[1rem] md:text-[1.25rem] order-2 md:order-3">
-              <span>Amsterdam&apos;s</span>
-              <span>Largest</span>
-              <span>Recording</span>
-              <span>Studio</span>
-            </p>
-          </div>
-        </div>
-      </Container>
+      </div>
+    </Container>
     </section>
   );
 };
